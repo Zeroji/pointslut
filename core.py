98f306e5d015c9267224a1c92510a46353fbdbe8
Zeroji <zzeroji@gmail.com>
2018-01-06 00:25:21 +0100

    Added API usage logging

--- core.py
+++ core.py
@@ -1,15 +1,32 @@
 """Core utilities."""
+import json
 import os
 import time
 import requests
 
 API = 'https://api.imgur.com/3/'
 MAX_ATTEMPTS = 5
+USAGE_LOG = 'usage.log'
+
+
+def log_usage(request):
+    """Log API usage information."""
+    now = int(time.time())
+    client_remaining = request.headers.get('X-RateLimit-ClientRemaining')
+    user_remaining = request.headers.get('X-RateLimit-UserRemaining')
+    reset = request.headers.get('X-RateLimit-UserReset')
+    if not all((client_remaining, user_remaining, reset)):
+        return
+    delay = reset - now
+    with open(USAGE_LOG, 'a') as log_file:
+        log_file.write(','.join(map(str, (now, client_remaining,
+                                          user_remaining, delay))))
+        log_file.write('\n')
 
 
 class Session:
     """Various token-based methods."""
-    def __init__(self, token, bearer=True):
+    def __init__(self, token, bearer=True, log=True):
         if os.path.isfile(token):
             with open(token, 'r') as token_file:
                 token = token_file.read().strip()
@@ -20,6 +37,7 @@ class Session:
                 self.token = f'Bearer {token}'
             else:
                 self.token = f'Client-ID{token}'
+        self.log = log
 
     def _request(self, method, url, **kwargs):
         """Perform an API request."""
@@ -30,6 +48,8 @@ class Session:
                 time.sleep(2**attempt)
                 attempt += 1
                 continue
+            if self.log:
+                log_usage(req)
             return req.json()
         return {'success': False}
 
